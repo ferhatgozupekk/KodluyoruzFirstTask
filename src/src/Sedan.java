fe6cd3b4a5c193b642aab7f1c142c771a83d3d06
Ferhat GÖZÜPEK <ferhatgozupek@gmail.com>
2023-06-09 20:24:44 +0300

    first
    first

---
+++ src/src/Sedan.java
@@ -0,0 +1,26 @@
+package src;
+public class Sedan {
+	public String color,name;
+	public int model, dailyPrice;
+	
+	Sedan(String color,String name,int dailyPrice,int model){
+		this.color = color;
+		this.name = name;
+		this.model = model;
+		this.dailyPrice = dailyPrice;
+	}
+
+	public double monthlyPrice(){
+		if (this.model < 2015 ) {
+			return 30 * (this.dailyPrice * 0.70);
+		}
+		else if (this.model >= 2015 || this.model < 2020) {
+			return 30 * (this.dailyPrice * 0.80);
+		}
+		else {
+			return 30 * (this.dailyPrice * 0.90);
+		}
+	}
+	
+}
+
